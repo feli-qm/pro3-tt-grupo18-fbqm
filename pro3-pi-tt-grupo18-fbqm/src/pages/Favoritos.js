23860517690576a33b1cc2872879ce80a5ef0821
feli-qm <fquirogamange@udesa.edu.ar>
2024-09-16 21:06:50 -0300

    Avances de favs: faltan corregir cosas

--- pro3-pi-tt-grupo18-fbqm/src/pages/Favoritos.js
+++ pro3-pi-tt-grupo18-fbqm/src/pages/Favoritos.js
@@ -4,27 +4,28 @@ class Favoritos extends Component {
   constructor(props) {
     super(props)
 
-    this.setState = {
+    this.state = {
       movies: [],
       isLoading: true
     }
   }
 
   componentDidMount() {
-    this.setState
+    this.setState = ({
+      isLoading: true
+    })
+    const storage = localStorage.getItem('favoritos')
     const parsedArray = JSON.parse(storage)
     Promise.all(
-
-      parsedArray.map((id) => {
-        //COMPLETAR
-        fetch('URL de movie')
-          .then(response => response.json())
+      parsedArray.map((id)=> {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=56c25df0bc04ec0dd18325a8ea74e10c`)
+            .then(response => response.json())
           .then(movie =>
             this.setState({
               movies: [...this.state.movies, movie]
             })
           )
-      })
+      }) 
     )
     this.setState({
       isLoading: false
@@ -33,9 +34,11 @@ class Favoritos extends Component {
 
   render() {
     return (
-      <div>{!this.state.isLoading ? "GRILLA" : "Loading..."} </div>
+      <div>
+        {!this.state.isLoading ? <p>GRILLA</p> : <p>Loading...</p>}
+      </div>
     )
   }
 }
 
-        export default Favoritos
+export default Favoritos
