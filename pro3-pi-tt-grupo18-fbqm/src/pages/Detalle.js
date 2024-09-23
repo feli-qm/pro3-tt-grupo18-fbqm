f88ea8da3f38f6bf5a8699bffb62eb5cfd9cfe1a
tfontanbalestra <tfontanbalestra@udesa.edu.ar>
2024-09-23 01:08:35 -0300

    Terminado el punto 5 (el detalle de las peliculas)

--- pro3-pi-tt-grupo18-fbqm/src/pages/Detalle.js
+++ pro3-pi-tt-grupo18-fbqm/src/pages/Detalle.js
@@ -3,8 +3,8 @@ import DetallePelicula from "../components/DetallePelicula/DetallePelicula"
 const Detalle = (props) => {
   return(
     <>
-    <h2>Detalle de la pelicula</h2>
-    <DetallePelicula match={props.match} />
+      <h2>Detalle de la pelicula</h2>
+      <DetallePelicula match={props.match} />
     </>
   )
 }
