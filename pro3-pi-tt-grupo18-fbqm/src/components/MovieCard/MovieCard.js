92628359086e9db052ef19b6af6148ecc0a2ed8b
tfontanbalestra <tfontanbalestra@udesa.edu.ar>
2024-09-23 16:52:55 -0300

    Correccion merge

--- pro3-pi-tt-grupo18-fbqm/src/components/MovieCard/MovieCard.js
+++ pro3-pi-tt-grupo18-fbqm/src/components/MovieCard/MovieCard.js
@@ -63,6 +63,7 @@ class MovieCard extends Component {
     }
 
     render(){
+        console.log(this.props.movies);
         
         const {id, title, poster_path, overview} = this.props.movies;
         const imgUrl = `https://image.tmdb.org/t/p/w500${poster_path}`;
@@ -71,19 +72,11 @@ class MovieCard extends Component {
             <article className="movie-card">
                 <div className='movieCard-content'>
                     <Link to={`/detalle/id/${id}`}><img src={imgUrl} alt=""/></Link><br />
-                    <Link to={`/detalle/id/${id}`}><h2>{title}</h2></Link>
-                    
-                    <button onClick={() => this.handleShowExtra()}>
-                        {this.state.showExtra ? "Ocultar descripcion": "Ver descripcion"}
-                    </button><br />
-
-                    <p className={this.state.showExtra ? "show":"hide"}>{overview}</p>
-                    
+                    <Link to={`/detalle/id/${id}`}><h2>{title}</h2></Link><br />
+                    <p className={this.state.showExtra ? "show":"hide"}>{overview}</p><br />
+                    <button onClick={() => this.handleShowExtra()}>{this.state.showExtra ? "Ocultar descripcion": "Ver descripcion"}</button><br />
                     <Link to={`/detalle/id/${id}`}><button>Ir a detalle</button></Link><br />
-
-                    <button onClick={()=> !this.state.esFavorito ? this.agregarFavorito() : this.sacarFavorito ()}>
-                        {!this.state.esFavorito ? "Agregar a favoritos" : "Quitar de favoritos"}
-                    </button><br />
+                    <button onClick={()=> !this.state.esFavorito ? this.agregarFavorito() : this.sacarFavorito ()}>{!this.state.esFavorito ? "Agregar a favoritos" : "Quitar de favoritos"}</button><br />
                 </div>
             </article>
         )
