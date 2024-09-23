d4e2c21ef0f4f7b91d6d31f6739311ffce5c4ad0
feli-qm <fquirogamange@udesa.edu.ar>
2024-09-23 15:18:26 -0300

    Implementado button cargar mas

--- pro3-pi-tt-grupo18-fbqm/src/pages/EnCartel.js
+++ pro3-pi-tt-grupo18-fbqm/src/pages/EnCartel.js
@@ -6,20 +6,25 @@ class EnCartel extends Component {
   constructor(props){
       super(props)
       this.state = {
-          movielist: []
+          movielist: [],
+          page: 1
       }
   }
 
   componentDidMount(){
+    this.loadMovies()
+}
 
-    const apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key=56c25df0bc04ec0dd18325a8ea74e10c';
+loadMovies(){
+  const apiUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=56c25df0bc04ec0dd18325a8ea74e10c&page=${this.state.page}`; // prop para decidir entre popular y cartelera para reutilizar el comp
 
     fetch(apiUrl, options)
     .then(response => response.json())
     .then(data => {
         const numeroPelis = this.props.limit !== undefined ? this.props.limit : data.results.length;
         this.setState({
-            movielist: data.results.slice(0,numeroPelis)
+            movielist: data.results.slice(0,numeroPelis),
+            page: this.state.page+1
         })
     })
     .catch(err => console.error(err))
@@ -31,6 +36,7 @@ render(){
       <div>
         <h2>Peliculas en cartel</h2>
         <MovieGrid movies={this.state.movielist}/>
+        <button onClick={()=>this.loadMovies()}>Cargar mas</button>
       </div>
     </>
   )
--- pro3-pi-tt-grupo18-fbqm/src/pages/MasPopulares.js
+++ pro3-pi-tt-grupo18-fbqm/src/pages/MasPopulares.js
@@ -6,20 +6,26 @@ class MasPopulares extends Component {
   constructor(props){
       super(props)
       this.state = {
-          movielist: []
+          movielist: [],
+          page: 1
       }
   }
 
   componentDidMount(){
 
-    const apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=56c25df0bc04ec0dd18325a8ea74e10c&language=en-US&page=1'; // prop para decidir entre popular y cartelera para reutilizar el comp
+   this.loadMovies()
+}
+
+loadMovies(){
+  const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=56c25df0bc04ec0dd18325a8ea74e10c&language=en-US&page=${this.state.page}`; // prop para decidir entre popular y cartelera para reutilizar el comp
 
     fetch(apiUrl, options)
     .then(response => response.json())
     .then(data => {
         const numeroPelis = this.props.limit !== undefined ? this.props.limit : data.results.length;
         this.setState({
-            movielist: data.results.slice(0,numeroPelis)
+            movielist: data.results.slice(0,numeroPelis),
+            page: this.state.page+1
         })
     })
     .catch(err => console.error(err))
@@ -31,6 +37,7 @@ render(){
       <div>
         <h2>Peliculas mas populares</h2>
         <MovieGrid movies={this.state.movielist}/>
+        <button onClick={()=>this.loadMovies()}>Cargar mas</button>
       </div>
     </>
   )
